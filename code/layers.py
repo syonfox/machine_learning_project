270179fd7e78fa5746f8a2724a43703adf140a94
syonfox <contact@kierlindsay.ca>
2018-12-06 22:58:36 -0800

    bug fix

--- code/layers.py
+++ code/layers.py
@@ -8,13 +8,28 @@ from keras import initializers
 from keras.layers import ZeroPadding2D, Layer, InputSpec
 
 # Extending the ZeroPadding2D layer to do reflection padding instead.
-class ReflectionPadding2D(ZeroPadding2D):
+# class ReflectionPadding2D(ZeroPadding2D):
+#     def call(self, x, mask=None):
+#         pattern = [[0, 0],
+#                    [self.top_pad, self.bottom_pad],
+#                    [self.left_pad, self.right_pad],
+#                    [0, 0]]
+#         return tf.pad(x, pattern, mode='REFLECT')
+
+class ReflectionPadding2D(Layer):
+    def __init__(self, padding=(1, 1), **kwargs):
+        self.padding = tuple(padding)
+        self.input_spec = [InputSpec(ndim=4)]
+        super(ReflectionPadding2D, self).__init__(**kwargs)
+
+    def get_output_shape_for(self, s):
+        """ If you are using "channels_last" configuration"""
+        return (s[0], s[1] + 2 * self.padding[0], s[2] + 2 * self.padding[1], s[3])
+
     def call(self, x, mask=None):
-        pattern = [[0, 0],
-                   [self.top_pad, self.bottom_pad],
-                   [self.left_pad, self.right_pad],
-                   [0, 0]]
-        return tf.pad(x, pattern, mode='REFLECT')
+        w_pad,h_pad = self.padding
+        return tf.pad(x, [[0,0], [h_pad,h_pad], [w_pad,w_pad], [0,0] ], 'REFLECT')
+
 
 
 class InstanceNormalization(Layer):
